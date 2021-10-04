f177e47dba370d229e58457fcb7ad20f0a8d1bf7
salts <83664500+saltes@users.noreply.github.com>
2021-10-04 00:34:45 +0000

    Update miui.sh

--- apps_clean/miui.sh
+++ apps_clean/miui.sh
@@ -9,18 +9,10 @@ apps_dir="
 CameraTools
 Mipay
 MiuiBugReport
-Browser
-Calendar
 MiuiCamera
-MiuiGallery
-MiuiVideo
-Music
-QuickSearchBox
 talkback
 "
 
 for delete_dir in $apps_dir ;do
   find $systemdir -type d -name "$delete_dir" | xargs rm -rf
 done
-
-rm -rf $1/data-app/*
